43e2270a5dc36250fd4889dcf78bcab899af43fa
AvanRocks <64660241+AvanRocks@users.noreply.github.com>
2020-10-22 01:03:47 -0400

    fixing bug

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8000
 const { Client } = require('pg')
 
 const client = new Client({
-	connectionString: process.env.DATABASE_URL,
+	connectionString: process.env.DATABASE_URL + '?sslmode=require',
 	ssl: {
     rejectUnauthorized: false
   }
